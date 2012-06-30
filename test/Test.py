c6b8448672012929c561da0f32e3abbea0d8fa41
Kevin Tang <tangkevin@utexas.edu>
2012-06-30 15:47:19 -0500

    Setup unit tests

---
+++ test/Test.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        self.assertTrue(False)
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
