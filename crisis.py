b03c6a60f063eafcdc9695562df1cdf4cbbd47d8
cwb658 <bowmancw6@gmail.com>
2012-07-16 15:32:06 -0500

    Update master

--- crisis.py
+++ crisis.py
@@ -15,12 +15,33 @@ from google.appengine.ext import db
 class CrisisHandler(blobstore_handlers.BlobstoreDownloadHandler):
     def get(self, resource):
         crisisref = str(urllib.unquote(resource))
-        crises = Crisis.all()
-        crises.filter('crisis_id =',crisisref)
-        c = crises.get()
-        print c.name
-        # path = os.path.join(os.path.dirname(__file__), 'crisis.html')
-        # self.response.out.write(template.render(path, template_values))
+        cris = Person.all()
+        infos = Info.all()
+        times = Time.all()
+        refs = Ref.all()
+        cris.filter('crisis_id =',crisisref)
+        p = cris.get()
+        crisis_list = {}
+        crisis_list["crisis"]=p
+        # crisis - Info
+        i = infos.ancestor(p).get()
+        crisis_list["info"]=i
+        # crisis - Info - Contact
+        crisis_list["time"]=times.ancestor(i).get()
+        # crisis - Ref
+        crisis_list["pimage"]=refs.ancestor(p).filter('ref_type =','primaryImage').get()
+        refs = Ref.all()
+        crisis_list["images"]=refs.ancestor(p).filter('ref_type =','image').run()
+        refs = Ref.all()
+        crisis_list["socials"]=refs.ancestor(p).filter('ref_type =','social').run()
+        refs = Ref.all()
+        crisis_list["videos"]=refs.ancestor(p).filter('ref_type =','video').run()
+        refs = Ref.all()
+        crisis_list["exts"]=refs.ancestor(p).filter('ref_type =','ext').run()
+        template_values = {
+            'crisis':crisis_list }
+        path = os.path.join(os.path.dirname(__file__), 'crisis.html')
+        self.response.out.write(template.render(path, template_values))
          
 
 def main():
