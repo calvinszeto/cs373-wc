da197a6feace139a6dec40dde442c0b6b620dbe4
Calvin Szeto at ENS <szeto.calvin@utexas.edu>
2012-07-18 14:28:04 -0500

    Fixed templates.

--- src/goodbyeworld.py
+++ src/goodbyeworld.py
@@ -7,6 +7,8 @@
 #
 
 import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 import urllib
 from Models import *
 
--- src/org.py
+++ src/org.py
@@ -2,6 +2,8 @@
 #
 
 import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 import urllib
 from Models import *
 
