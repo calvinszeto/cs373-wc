735d7ba1a90908352699e086d4ff9eb7f642d21c
Calvin Szeto at ENS <szeto.calvin@utexas.edu>
2012-07-29 18:36:56 -0500

    Add AND/OR to search function.

--- src/testQuery.py
+++ src/testQuery.py
@@ -2,14 +2,15 @@
 #
 
 #
-# goodbyeworld.py
-# Handles the Export feature
+# testQuery.py
+# Handles the Search Bar Function
 #
 
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 import urllib
+import re
 from Models import *
 
 from xml.etree.ElementTree import ElementTree
@@ -93,26 +94,39 @@ class SearchHandler(webapp.RequestHandler):
             options = search.QueryOptions(
                 returned_fields=['name', 'history', 'idref'],
                 snippeted_fields=['history'])
-            query = search.Query(query_string=self.request.get("q"), options=options)
+            and_query_string = self.request.get("q")
+            or_query_string = re.sub('\s+', ' OR ',and_query_string)
+            and_query = search.Query(query_string=and_query_string, options=options)
+            or_query = search.Query(query_string=or_query_string, options=options)
             index = search.Index(name=_INDEX_NAME)
 
             # Execute the query
-            results = index.search(query)
+            and_results = index.search(and_query)
+            or_results = index.search(or_query)
+            received_ids = []
 
-            result_list = [] 
+            and_result_list = [] 
+            or_result_list = []
             refs = Ref.all()
 
-            for scored_document in results:
-                index = len(result_list)
-                result_list.append({}) 
+            received = False
+
+            for scored_document in and_results:
+                index = len(and_result_list)
+                and_result_list.append({}) 
                 doc_type = " "
                 id_ref = " "
                 # self.response.out.write(scored_document.fields[0].value)
                 for fields in scored_document.fields:
                     if fields.name == "idref":
                         id_ref = fields.value
+                        if id_ref in received_ids:
+                            received = True 
+                        else:
+                            received = False
+                            received_ids.append(id_ref)
                     else:
-                        result_list[index][fields.name] = fields.value
+                        and_result_list[index][fields.name] = fields.value
                     # self.response.out.write(str(fields.value) + " ")
                 ref = None
                 if "crisis" in scored_document.doc_id:
@@ -124,13 +138,52 @@ class SearchHandler(webapp.RequestHandler):
                 if "person" in scored_document.doc_id:
                     ref = refs.ancestor(Person.all().filter('person_id =',id_ref).get()).filter('ref_type =', 'primaryImage').get()
                     doc_type = "person"
-                result_list[index]["image"] = ref.url
+                and_result_list[index]["image"] = ref.url
                 link = "/%s/%s" % (doc_type,id_ref)
                 # self.response.out.write(link)
-                result_list[index]["link"] = link
+                and_result_list[index]["link"] = link
+                if received:
+                    and_result_list.remove(and_result_list[index])
+
+            for scored_document in or_results:
+                index = len(or_result_list)
+                or_result_list.append({}) 
+                doc_type = " "
+                id_ref = " "
+                # self.response.out.write(scored_document.fields[0].value)
+                for fields in scored_document.fields:
+                    if fields.name == "idref":
+                        id_ref = fields.value
+                        if id_ref in received_ids:
+                            received = True 
+                        else:
+                            received = False
+                            received_ids.append(id_ref)
+                    else:
+                        or_result_list[index][fields.name] = fields.value
+                    # self.response.out.write(str(fields.value) + " ")
+                ref = None
+                if "crisis" in scored_document.doc_id:
+                    doc_type = "crisis"
+                    ref = refs.ancestor(Crisis.all().filter('crisis_id =',id_ref).get()).filter('ref_type =', 'primaryImage').get()
+                if "org" in scored_document.doc_id:
+                    ref = refs.ancestor(Organization.all().filter('org_id =',id_ref).get()).filter('ref_type =', 'primaryImage').get()
+                    doc_type = "org"
+                if "person" in scored_document.doc_id:
+                    ref = refs.ancestor(Person.all().filter('person_id =',id_ref).get()).filter('ref_type =', 'primaryImage').get()
+                    doc_type = "person"
+                or_result_list[index]["image"] = ref.url
+                link = "/%s/%s" % (doc_type,id_ref)
+                # self.response.out.write(link)
+                or_result_list[index]["link"] = link
+                if received:
+                    or_result_list.remove(or_result_list[index])
                 
             template_values = {
-                    'results':result_list
+                    'and_results':and_result_list,
+                    'num_and':len(and_result_list),
+                    'or_results':or_result_list,
+                    'num_or':len(or_result_list),
                 }
             path = os.path.join(os.path.dirname(__file__), '../templates/searchresults.html')
             self.response.out.write(template.render(path, template_values))
